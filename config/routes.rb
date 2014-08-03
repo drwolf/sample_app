e7699d13b3e8e650177df6a8d8a184b27f5e6b63
Robert Wolf <derrobertwolf@gmx.de>
2014-08-03 12:13:50 +0200

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 SampleApp::Application.routes.draw do
-resources :users 
+  get "relationships/create"
+
+  get "relationships/destroy"
+
+  get "relationship/create"
+
+  get "relationship/destroy"
+
+resources :users do
+  member do
+    get :following, :followers
+  end
+end
 resources :sessions, only: [:new, :create, :destroy] 
 resources :microposts, only: [:create, :destroy] 
+resources :relationships, only: [:create, :destroy]
 
 root to: 'static_pages#home'
+
   #match 'sessions', to: 'sessions#new', 	via: 'get'
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
